5920b19d35642a355fae8e02e564bf5d5c4e53c0
Sovichea Cheth <52162630+sovichea19@users.noreply.github.com>
2019-09-21 14:02:38 -0500

    KafkaDemo

---
+++ KafkaDemo/src/test/java/cs523/YouTubeVideoTrendingKafka/AppTest.java
@@ -0,0 +1,20 @@
+package cs523.YouTubeVideoTrendingKafka;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
