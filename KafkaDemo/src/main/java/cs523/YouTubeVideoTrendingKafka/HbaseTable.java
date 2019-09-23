5dcef251eaad02710ab6f142feb4a3245b3cc3b0
sureshprajapati076 <51094657+sureshprajapati076@users.noreply.github.com>
2019-09-22 19:48:57 -0500

    Kafka Streaming Demo

--- KafkaDemo/src/main/java/cs523/YouTubeVideoTrendingKafka/HbaseTable.java
+++ KafkaDemo/src/main/java/cs523/YouTubeVideoTrendingKafka/HbaseTable.java
@@ -16,51 +16,44 @@ import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class HbaseTable {
-	private static final String TABLE_NAME = "videos";
+	private static final String TABLE_NAME = "videos_kafka";
 	
-	private static final String	ABOUT = "CF_About";
-	private static final String DETAILS = "CF_Details";
-	private static final String	COMMENT ="CF_Comments";
-	private static final String	SETTINGS ="CF_Settings";
-	private static final String REACTION = "CF_Reaction";
-	private static final String TRENDING = "CF_Trending";
-	//private static final String ROW_ID ="CF_RowId";
-			
+	private static final String CF_DETAILS = "details";
+	private static final String	CF_SETTINGS ="settings";
+	private static final String CF_REACTION = "reaction";
 	
 	private static final Configuration config = HBaseConfiguration.create();
 
-	@SuppressWarnings("deprecation")
 	public static void CreateTable() throws IOException {
 		
 		try (Connection connection = ConnectionFactory.createConnection(config);
 				Admin admin = connection.getAdmin()) {
 			HTableDescriptor table = new HTableDescriptor(
 					TableName.valueOf(TABLE_NAME));
-			table.addFamily(new HColumnDescriptor(DETAILS));
-			table.addFamily(new HColumnDescriptor(ABOUT));
-			table.addFamily(new HColumnDescriptor(REACTION));
 			
-			table.addFamily(new HColumnDescriptor(COMMENT));
-			table.addFamily(new HColumnDescriptor(SETTINGS));
-			table.addFamily(new HColumnDescriptor(TRENDING));
+			table.addFamily(new HColumnDescriptor(CF_DETAILS));
+			
+			table.addFamily(new HColumnDescriptor(CF_REACTION));
 			
 			
-			
+			table.addFamily(new HColumnDescriptor(CF_SETTINGS));
+		
 			
 			
 			
+			
+		//	admin.disableTable(table.getTableName());
+			//admin.deleteTable(table.getTableName());
 			
 			
 			System.out.print("Creating table.... ");
-			if (!admin.tableExists(table.getTableName())) {
-		
-			admin.createTable(table);
-			}
-			
-			/*		admin.disableTable(table.getTableName());
+			if (admin.tableExists(table.getTableName())) {
+				
+				admin.disableTable(table.getTableName());
 				admin.deleteTable(table.getTableName());
 			}
-			else{*/
+			admin.createTable(table);
+			
 			System.out.println(" Done!");
 		}
 	}
@@ -68,31 +61,30 @@ public class HbaseTable {
 		@SuppressWarnings("deprecation")
 		public static void addData(List<String> list) throws IOException{	
 			//System.out.println(list);
+			if(list.size()==16){
 			
-			
-			HTable hTable = new HTable(config, "videos");
+			HTable hTable = new HTable(config, "videos_kafka");
 			Put p1 = new Put(Bytes.toBytes(list.get(0)));
 			
-			p1.add(Bytes.toBytes(DETAILS), Bytes.toBytes("title"),Bytes.toBytes(list.get(2)));
-			p1.add(Bytes.toBytes(DETAILS), Bytes.toBytes("published_date"),Bytes.toBytes(list.get(5)));
-			p1.add(Bytes.toBytes(DETAILS), Bytes.toBytes("thumbnail_link"),Bytes.toBytes(list.get(11)));
+			p1.add(Bytes.toBytes(CF_DETAILS), Bytes.toBytes("title"),Bytes.toBytes(list.get(2)));
+			p1.add(Bytes.toBytes(CF_DETAILS), Bytes.toBytes("published_date"),Bytes.toBytes(list.get(5)));
+			p1.add(Bytes.toBytes(CF_DETAILS), Bytes.toBytes("thumbnail_link"),Bytes.toBytes(list.get(11)));
+			p1.add(Bytes.toBytes(CF_DETAILS), Bytes.toBytes("description"),Bytes.toBytes(list.get(15)));
+			p1.add(Bytes.toBytes(CF_DETAILS), Bytes.toBytes("channel_title"),Bytes.toBytes(list.get(3)));
+			p1.add(Bytes.toBytes(CF_DETAILS), Bytes.toBytes("category_id"),Bytes.toBytes(list.get(4)));
+			p1.add(Bytes.toBytes(CF_DETAILS), Bytes.toBytes("tags"),Bytes.toBytes(list.get(6)));
+			p1.add(Bytes.toBytes(CF_DETAILS), Bytes.toBytes("trending_date"),Bytes.toBytes(list.get(1)));
 			
-			p1.add(Bytes.toBytes(ABOUT), Bytes.toBytes("description"),Bytes.toBytes(list.get(15)));
-			p1.add(Bytes.toBytes(ABOUT), Bytes.toBytes("channel_title"),Bytes.toBytes(list.get(3)));
-			p1.add(Bytes.toBytes(ABOUT), Bytes.toBytes("category_id"),Bytes.toBytes(list.get(4)));
-			p1.add(Bytes.toBytes(ABOUT), Bytes.toBytes("tags"),Bytes.toBytes(list.get(6)));
-			
-			p1.add(Bytes.toBytes(REACTION), Bytes.toBytes("views"),Bytes.toBytes(list.get(7)));
-			p1.add(Bytes.toBytes(REACTION), Bytes.toBytes("likes"),Bytes.toBytes(list.get(8)));
-			p1.add(Bytes.toBytes(REACTION), Bytes.toBytes("dislikes"),Bytes.toBytes(list.get(9)));
-			
-			p1.add(Bytes.toBytes(COMMENT), Bytes.toBytes("comment_count"),Bytes.toBytes(list.get(10)));
+			p1.add(Bytes.toBytes(CF_REACTION), Bytes.toBytes("views"),Bytes.toBytes(list.get(7)));
+			p1.add(Bytes.toBytes(CF_REACTION), Bytes.toBytes("likes"),Bytes.toBytes(list.get(8)));
+			p1.add(Bytes.toBytes(CF_REACTION), Bytes.toBytes("dislikes"),Bytes.toBytes(list.get(9)));
+			p1.add(Bytes.toBytes(CF_REACTION), Bytes.toBytes("comment_count"),Bytes.toBytes(list.get(10)));
 		
-			p1.add(Bytes.toBytes(SETTINGS), Bytes.toBytes("comment_disable"),Bytes.toBytes(list.get(12)));
-			p1.add(Bytes.toBytes(SETTINGS), Bytes.toBytes("rating_disable"),Bytes.toBytes(list.get(13)));
-			p1.add(Bytes.toBytes(SETTINGS), Bytes.toBytes("video_removed_err"),Bytes.toBytes(list.get(14)));
+			p1.add(Bytes.toBytes(CF_SETTINGS), Bytes.toBytes("comment_disable"),Bytes.toBytes(list.get(12)));
+			p1.add(Bytes.toBytes(CF_SETTINGS), Bytes.toBytes("rating_disable"),Bytes.toBytes(list.get(13)));
+			p1.add(Bytes.toBytes(CF_SETTINGS), Bytes.toBytes("video_removed_err"),Bytes.toBytes(list.get(14)));
 			
-			p1.add(Bytes.toBytes(TRENDING), Bytes.toBytes("trending_date"),Bytes.toBytes(list.get(1)));
+		
 		
 			
 			
@@ -103,6 +95,10 @@ public class HbaseTable {
 			System.out.println("data inserted");
 
 			hTable.close();
+			}
+			else{
+				System.out.println("Skipped: (size) = "+list.size()+".....>"+list);
+			}
 			
 		}
 	
--- KafkaDemo/src/main/java/cs523/YouTubeVideoTrendingKafka/KafkaDemo.java
+++ KafkaDemo/src/main/java/cs523/YouTubeVideoTrendingKafka/KafkaDemo.java
@@ -3,32 +3,29 @@ package cs523.YouTubeVideoTrendingKafka;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Stream;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.streaming.kafka010.KafkaUtils;
-import org.apache.spark.streaming.kafka010.LocationStrategies;
-import org.apache.spark.streaming.kafka010.OffsetRange;
 
 public class KafkaDemo {
 	
 	private static String KafkaTopic = null;
 	private static String CsvFile = null;
+	public static long count=0;
 	
 	public static void main(String[] args) throws IOException, ExecutionException, InterruptedException{
+		HbaseTable.CreateTable();
+		
+		System.out.println("Starting file "+args[1]);
 		
 		if (args != null) {
 			KafkaTopic = args[0];
@@ -37,10 +34,6 @@ public class KafkaDemo {
 		
 		KafkaDemo kafkaProducer = new KafkaDemo();
 		kafkaProducer.PublishMessage();
-		
-//		startKafka();
-//		sparkStream();
-//		System.out.println("done tested...");
 		
 	}
 	
@@ -59,63 +52,45 @@ public class KafkaDemo {
 	}
 	
 	//Publish method to read the rows from csv and write the message
-	private void PublishMessage() {
+	private void PublishMessage() throws InterruptedException {
 		final Producer<String, String> CsvProducer = ProducerProperties();
-		
-		try {
-			Stream<String> FileStream = Files.lines(Paths.get(CsvFile));
+		while(true){
 			
-			FileStream.forEach(line -> {
-				final ProducerRecord<String, String> CsvRecord = new ProducerRecord<String, String>(KafkaTopic, UUID.randomUUID().toString(), line);
 			
-				CsvProducer.send(CsvRecord, (metadata, exception) -> {
-					if(metadata != null) {
-						System.out.println("CsvData: ->" + CsvRecord.key() + "|" + CsvRecord.value());
-						
-						
-						
-					} else {
-						System.out.println("Error Sending CSV Record -> " + CsvRecord.value());
-					}
+
+			try (Stream<String> FileStream = Files.lines(Paths.get(CsvFile)).skip(count)) {
+				
+				
+				FileStream.forEach(line -> {
+					count++;
+					final ProducerRecord<String, String> CsvRecord = new ProducerRecord<String, String>(KafkaTopic, UUID.randomUUID().toString(), line);
+				
+					CsvProducer.send(CsvRecord, (metadata, exception) -> {
+						if(metadata != null) {
+							System.out.println("CsvData: ->" + CsvRecord.key() + "|" + CsvRecord.value());
+							List<String> row = Arrays.asList(CsvRecord.value().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"));
+							try {
+								HbaseTable.addData(row);
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+						} else {
+							System.out.println("Error Sending CSV Record -> " + CsvRecord.value());
+						}
+					});
 				});
-			});
-		} catch (IOException e) {
-			e.printStackTrace();
+				
+				FileStream.close();
+				
+				
+				
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
-//	//Spark Stream method
-//	@SuppressWarnings("unchecked")
-//	public static void sparkStream() {
-//		OffsetRange[] offsetRanges = {
-//			//Topic, partition, starting offset, ending offset
-//			OffsetRange.create("YTVideos", 0, 0, 100),
-//			OffsetRange.create("YTVideos",  1, 0, 100)
-//		};
-//		
-//		JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("YouTubeVideoTrending").setMaster("local"));
-//		
-//        JavaRDD<ConsumerRecord<String, String>> rdd = KafkaUtils.createRDD(
-//                sc,
-//                (Map<String, Object>) ProducerProperties(),
-//                offsetRanges,
-//                LocationStrategies.PreferConsistent()
-//        );
-//	}
-//	
-//    public static void startKafka() throws ExecutionException, InterruptedException {
-//        Properties kProp = new Properties();
-//        kProp.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-//        kProp.put(ProducerConfig.CLIENT_ID_CONFIG, "kafkaStreamer");
-//        kProp.put(ProducerConfig.ACKS_CONFIG, "all");
-//        kProp.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.LongSerializer");
-//        kProp.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-//        KafkaProducer<Long, String> streamProducer = new KafkaProducer<Long, String>(kProp);
-//        for(int i=0;i<10;i++){
-//            ProducerRecord<Long,String> data = new ProducerRecord<Long,String>("kafka-spark","testing  "+System.nanoTime());
-//            RecordMetadata rm = streamProducer.send(data).get();
-//            System.out.println("Topic:"+rm.topic()+":"+rm.offset());
-//        }
-//    }
 
 }
