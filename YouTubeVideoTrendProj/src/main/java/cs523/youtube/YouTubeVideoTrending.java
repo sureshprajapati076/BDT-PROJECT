552e0011096b3fd7a63495288f00434359b12425
SunenaGwachha <51094691+SunenaGwachha@users.noreply.github.com>
2019-09-22 19:43:37 -0500

    Update YouTubeVideoTrending.java

--- YouTubeVideoTrendProj/src/main/java/cs523/youtube/YouTubeVideoTrending.java
+++ YouTubeVideoTrendProj/src/main/java/cs523/youtube/YouTubeVideoTrending.java
@@ -11,89 +11,26 @@ import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 public class YouTubeVideoTrending 
 {
-  
-
   public static void main(String[] args) throws InterruptedException, IOException 
   {
-	  
-	  
 	  HbaseTable.CreateTable();
-	  
-	// MyFirstHbaseTable.addData(new ArrayList<String>());
-	  
-	  
 	  JavaStreamingContext ssc = new JavaStreamingContext(new SparkConf().setAppName("wordCount").setMaster("local"), Durations.seconds(15));
-	  
-	  
 	  JavaDStream<String> lines = ssc.textFileStream("hdfs://localhost/user/cloudera/test");
-	
-	 
-		  
-		  
-		  
-		  
-		  
-		  
-		  
-	  
-		  
-	  
 	 JavaDStream<List<String>> counter = lines.map(line->Arrays.asList(line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)")));
-	  
-	  
-	  
-	  
 	  counter.foreachRDD(rdd ->{
-		  
-		  
 		  if(!rdd.isEmpty()) {
 		  System.out.println("Contents of added File are:\n");
-		  
-		  
 		  rdd.collect().forEach(x->{
-			  
-			  
-			 // System.out.println(x);
-			  
 			  try {
 				HbaseTable.addData(x);
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			  
-		  
-		  
-		  
-		  });
-		//  rdd.coalesce(1).saveAsTextFile("/home/cloudera/output");
-		  
-		  }
+		});
+	  }
       });
-	  
-	  
-	  
-	  
-	  
-	  
-	  
-	  ssc.start();
-	  
-	 
-
-	  
-	  
-	  
-	  
-	  
-	  ssc.awaitTermination();
-	  
-	  
-	  
-ssc.close();
-	  
-	  
-	 
-	 
-  }
+  	ssc.start();
+	ssc.awaitTermination();
+	ssc.close();
+	}
 }
